f062abc56643e36e0538d7c66c29531804791951
QuangThuan <tranquanthuan132@gmail.com>
2024-02-22 16:00:55 +0700

    update-server

--- Model/logins/index.js
+++ Model/logins/index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 
 const Login = new Schema({
+    "name":{type: String},
     email:{type: String},
     passWorld:{type:String}
 
